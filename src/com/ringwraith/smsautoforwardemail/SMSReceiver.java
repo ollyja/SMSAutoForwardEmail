2528517b3df4b6a10298678834935185c948e8ee
ringwraith <ringwraith@163.com>
2013-12-16 16:30:11 +0800

    enable email

--- src/com/ringwraith/smsautoforwardemail/SMSReceiver.java
+++ src/com/ringwraith/smsautoforwardemail/SMSReceiver.java
@@ -2,6 +2,7 @@ package com.ringwraith.smsautoforwardemail;
 
 import java.text.SimpleDateFormat;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.content.Context;
@@ -15,7 +16,7 @@ public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.e("onReceive", "Received!");
+		// Log.e("onReceive", "Received!");
 
 		if (intent.getAction().equals(RECEIVED)) {
 			Bundle bundle = intent.getExtras();
@@ -29,15 +30,61 @@ public class SMSReceiver extends BroadcastReceiver {
 
 				for (int i = 0; i < objpdus.length; i++) {
 					sms[i] = SmsMessage.createFromPdu((byte[]) objpdus[i]);
-					msg += "Time: " + datefmt.format(sms[i].getTimestampMillis()) + "\n";
+					msg += "Time: "
+							+ datefmt.format(sms[i].getTimestampMillis())
+							+ "\n";
 					msg += "From: " + sms[i].getOriginatingAddress() + "\n";
 					msg += "Content: " + sms[i].getMessageBody() + "\n";
-					Log.e("onReceive", msg);
-					
+					// Log.e("onReceive", msg);
+					SendTask send = new SendTask();
+					send.execute(msg);
+
 				}
 			}
 		}
 	}
-}
 
-	
+	class SendTask extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+
+			Email email = new Email("123456789@qq.com", "123456789");
+			email.setFrom("123456789@qq.com");
+			email.setTo(new String[] { "123456789@qq.com" });
+			email.setSubject("New SMS");
+			email.setBody(params[0]);
+
+			try {
+				// If you want add attachment use function addAttachment.
+				// email.addAttachment("/sdcard/filelocation");
+
+				if (email.send()) {
+					// Log.e("doInBackground", "Email was sent successfully.");
+				} else {
+					Log.e("doInBackground", "Sent Failed!");
+				}
+			} catch (Exception e) {
+				Log.e("doInBackground", "Exception!", e);
+			}
+
+			return "";
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			super.onProgressUpdate(progress);
+		}
+
+		@Override
+		protected void onPostExecute(String r) {
+			super.onPostExecute(r);
+		}
+	}
+}
